b37f9aabd23d7f5ffe4c09777d28b7f4ba47999a
mc-carthy <mccarthy.mick0012@gmail.com>
2017-09-07 22:42:41 +0100

    Create dropdown showing Genres

--- Gighub/Controllers/GigsController.cs
+++ Gighub/Controllers/GigsController.cs
@@ -1,13 +1,27 @@
-﻿using System.Web.Mvc;
+﻿using Gighub.Models;
+using Gighub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace Gighub.Controllers
 {
     public class GigsController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
         // GET: Gigs
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
         }
     }
 }
--- Gighub/ViewModels/GigFormViewModel.cs
+++ Gighub/ViewModels/GigFormViewModel.cs
@@ -1,9 +1,14 @@
-﻿namespace Gighub.ViewModels
+﻿using Gighub.Models;
+using System.Collections.Generic;
+
+namespace Gighub.ViewModels
 {
     public class GigFormViewModel
     {
         public string Date { get; set; }
         public string Time { get; set; }
         public string Venue { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
     }
 }
