5a0c60a6138bcc6f02ae29d8d44e1402caa2b4f8
mc-carthy <mccarthy.mick0012@gmail.com>
2017-09-07 22:22:58 +0100

    Create basic 'Create Gig' page

---
+++ Gighub/Controllers/GigsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Gighub.Controllers
+{
+    public class GigsController : Controller
+    {
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
