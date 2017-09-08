cc8bbbb61181000512a4bf75a67b1daa76538734
mc-carthy <mccarthy.mick0012@gmail.com>
2017-09-08 14:54:33 +0100

    Protect against CSRF attacks

--- Gighub/Controllers/GigsController.cs
+++ Gighub/Controllers/GigsController.cs
@@ -28,6 +28,7 @@ namespace Gighub.Controllers
 
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(GigFormViewModel viewModel)
         {
             if (!ModelState.IsValid)
