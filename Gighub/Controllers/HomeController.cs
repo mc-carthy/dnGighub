221d331eacd283b1871863edab329671209db00e
mc-carthy <mccarthy.mick0012@gmail.com>
2017-09-12 15:39:05 +0100

    Display relevant gig info on homepage

--- Gighub/Controllers/HomeController.cs
+++ Gighub/Controllers/HomeController.cs
@@ -20,6 +20,7 @@ namespace Gighub.Controllers
         {
             var upcomingGigs = _context.Gigs
                 .Include(g => g.Artist)
+                .Include(g => g.Genre)
                 .Where(g => g.DateTime > DateTime.Now);
 
             return View(upcomingGigs);
