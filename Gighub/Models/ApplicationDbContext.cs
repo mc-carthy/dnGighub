6d16c7e11f8a4adf235e9600b36bf2ec2042171f
mc-carthy <mccarthy.mick0012@gmail.com>
2017-09-12 16:25:36 +0100

    Create intermediatary Attendance model to simulate users<->gigs relationship

--- Gighub/Models/ApplicationDbContext.cs
+++ Gighub/Models/ApplicationDbContext.cs
@@ -7,6 +7,8 @@ namespace Gighub.Models
     {
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -15,6 +17,16 @@ namespace Gighub.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
