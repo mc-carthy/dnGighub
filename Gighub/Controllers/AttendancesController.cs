902c50fed6f538dd72216ebc2cb1efbb2650550c
mc-carthy <mccarthy.mick0012@gmail.com>
2017-09-12 22:18:34 +0100

    Add ability to add atendances via API

--- Gighub/Controllers/AttendancesController.cs
+++ Gighub/Controllers/AttendancesController.cs
@@ -1,13 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using Gighub.Models;
+using Microsoft.AspNet.Identity;
 using System.Web.Http;
 
 namespace Gighub.Controllers
 {
+    [Authorize]
     public class AttendancesController : ApiController
     {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int gigId)
+        {
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }
