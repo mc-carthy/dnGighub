8d7ad706549f2dbbca1c31fffed723e6afe7ab69
mc-carthy <mccarthy.mick0012@gmail.com>
2017-09-12 15:02:00 +0100

    Display all upcoming gigs on homepage

--- Gighub/Controllers/HomeController.cs
+++ Gighub/Controllers/HomeController.cs
@@ -1,16 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Gighub.Models;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Gighub.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcomingGigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Where(g => g.DateTime > DateTime.Now);
+
+            return View(upcomingGigs);
         }
 
         public ActionResult About()
