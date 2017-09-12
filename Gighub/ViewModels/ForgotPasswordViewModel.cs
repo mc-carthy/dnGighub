c86b74904f4536749d6eda6c9cdb2f77e014d4d5
mc-carthy <mccarthy.mick0012@gmail.com>
2017-09-12 15:28:57 +0100

    Extract ViewModels from 'Models' namespace & folder

---
+++ Gighub/ViewModels/ForgotPasswordViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gighub.ViewModels
+{
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
---
+++ Gighub/ViewModels/ForgotViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gighub.ViewModels
+{
+    public class ForgotViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
