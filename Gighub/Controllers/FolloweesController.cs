02e52a292e6252cf6d736f7167b5359380282f18
mc-carthy <mccarthy.mick0012@gmail.com>
2017-09-13 23:46:52 +0100

    Create Artists I'm Following page

---
+++ Gighub/Controllers/FolloweesController.cs
@@ -0,0 +1,28 @@
+﻿using Gighub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Gighub.Controllers
+{
+    public class FolloweesController : Controller
+    {
+        public ApplicationDbContext _context;
+
+        public FolloweesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var currentUserId = User.Identity.GetUserId();
+
+            var followees = _context.Followings
+                .Where(f => f.FollowerId == currentUserId)
+                .Select(f => f.Followee)
+                .ToList();
+
+            return View(followees);
+        }
+    }
+}
