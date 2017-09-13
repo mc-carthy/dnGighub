10b99ceaf22550b649f00782458a275e19ce72a1
mc-carthy <mccarthy.mick0012@gmail.com>
2017-09-13 19:39:27 +0100

    Only show Follow?/Going? buttons to logged in users

--- Gighub/Controllers/HomeController.cs
+++ Gighub/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Gighub.Models;
+using Gighub.ViewModels;
 using System;
 using System.Data.Entity;
 using System.Linq;
@@ -23,7 +24,13 @@ namespace Gighub.Controllers
                 .Include(g => g.Genre)
                 .Where(g => g.DateTime > DateTime.Now);
 
-            return View(upcomingGigs);
+            var viewModel = new HomeViewModel
+            {
+                UpcomingGigs = upcomingGigs,
+                ShowActions = User.Identity.IsAuthenticated
+            };
+
+            return View(viewModel);
         }
 
         public ActionResult About()
---
+++ Gighub/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Gighub.Models;
+using System.Collections.Generic;
+
+namespace Gighub.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Gig> UpcomingGigs { get; set; }
+        public bool ShowActions { get; set; }
+    }
+}
