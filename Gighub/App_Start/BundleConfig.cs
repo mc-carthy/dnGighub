390eee750597d8aa44b40ddd4b3848fdbc1d168c
mc-carthy <mccarthy.mick0012@gmail.com>
2017-09-14 18:26:27 +0100

    Replace standard confirm box with bootbox modal window

--- Gighub/App_Start/BundleConfig.cs
+++ Gighub/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Gighub
 {
@@ -20,8 +19,9 @@ namespace Gighub
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                        "~/Scripts/bootbox.min.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
