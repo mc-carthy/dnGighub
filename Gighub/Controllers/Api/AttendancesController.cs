6ca818b7b68dda111146943c6f059bf73e8fc9e6
mc-carthy <mccarthy.mick0012@gmail.com>
2017-09-14 21:28:42 +0100

    Organise Controllers/Api Controllers

--- Gighub/Controllers/Api/AttendancesController.cs
+++ Gighub/Controllers/Api/AttendancesController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNet.Identity;
 using System.Linq;
 using System.Web.Http;
 
-namespace Gighub.Controllers
+namespace Gighub.Controllers.Api
 {
     [Authorize]
     public class AttendancesController : ApiController
