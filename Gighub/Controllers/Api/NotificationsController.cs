4444f0549c5d88d94a3f012a5ac006dc9be89c10
mc-carthy <mccarthy.mick0012@gmail.com>
2017-09-15 23:07:28 +0100

    Create API to return info from cancelled gigs

---
+++ Gighub/Controllers/Api/NotificationsController.cs
@@ -0,0 +1,31 @@
+﻿using Gighub.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace Gighub.Controllers.Api
+{
+    [Authorize]
+    public class NotificationsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NotificationsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public IEnumerable<Notification> GetNewNotifications()
+        {
+            var currentUserId = User.Identity.GetUserId();
+            var notifications = _context.UserNotifications
+                .Where(un => un.UserId == currentUserId)
+                .Select(un => un.Notification)
+                .Include(n => n.Gig.Artist)
+                .ToList();
+
+            return notifications;
+        }
+    }
+}
