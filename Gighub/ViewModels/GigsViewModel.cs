74db5a9550521ba2f21dd87a1f6d8f1d83e0897e
mc-carthy <mccarthy.mick0012@gmail.com>
2017-09-19 21:59:09 +0100

    Create search box

--- Gighub/ViewModels/GigsViewModel.cs
+++ Gighub/ViewModels/GigsViewModel.cs
@@ -8,5 +8,6 @@ namespace Gighub.ViewModels
         public IEnumerable<Gig> UpcomingGigs { get; set; }
         public bool ShowActions { get; set; }
         public string Heading { get; set; }
+        public string SearchTerm { get; set; }
     }
 }
